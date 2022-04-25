24456bd0c8f2b027125ab325eeefced1c6f89169
Bharati646 <96813981+Bharati646@users.noreply.github.com>
2022-04-25 16:33:34 +0530

    Add files via upload

---
+++ First.ts
@@ -0,0 +1,13 @@
+let Id:number = 5464654; 
+let Name :string ='Scott';
+let job:string='Manager'
+let permanent_emp:boolean=true;
+let emp_salary :string ="INR.45000";
+let dept_no:number=10;
+
+console.log("Employee ID: "+ " " +Id);
+console.log("Employee Name: "+ " " +Name);
+console.log("Employee Job: "+ " " +job);
+console.log("Is Permanent Employee: "+ " " +permanent_emp);
+console.log("Employee Salary: "+ " " +emp_salary);
+console.log("Employee Deptno: "+ " " +dept_no);
