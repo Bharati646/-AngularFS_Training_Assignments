9b131625e0c8a11af27813b9a311d07ef851f24f
Bharati646 <96813981+Bharati646@users.noreply.github.com>
2022-05-03 01:58:47 +0530

    Add files via upload

---
+++ Typescript/Task1/First.js
@@ -0,0 +1,12 @@
+var Id = 5464654;
+var Name = 'Scott';
+var job = 'Manager';
+var permanent_emp = true;
+var emp_salary = "INR.45000";
+var dept_no = 10;
+console.log("Employee ID: " + " " + Id);
+console.log("Employee Name: " + " " + Name);
+console.log("Employee Job: " + " " + job);
+console.log("Is Permanent Employee: " + " " + permanent_emp);
+console.log("Employee Salary: " + " " + emp_salary);
+console.log("Employee Deptno: " + " " + dept_no);
